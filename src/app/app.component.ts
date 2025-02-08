3d1c688f58b58409e020a615f190ff3dfdf896ff
CISC181 <bgibbons@udel.edu>
2025-02-08 11:03:17 -0500

    handle output / event handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { UserComponent } from "./user/user.component";
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,11 @@ import { UserComponent } from "./user/user.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
+export class AppComponent {
+  users = DUMMY_USERS;
 
+  onSelectUser(id: string) {
+    console.log('Selected user with id ' + id);
 
-
-
-
-
-export class AppComponent {}
+  }
+}
