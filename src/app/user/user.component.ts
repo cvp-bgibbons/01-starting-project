cc4e008f3e0f2f49724d7e012d10f6119b245462
CISC181 <bgibbons@udel.edu>
2025-02-08 14:24:47 -0500

    importing full user record

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -9,19 +9,27 @@ import { Component, EventEmitter, input, Input, Output } from '@angular/core';
 })
 export class UserComponent {
   //  Required:true means the html requires the attribute!
-  @Input ({required:true}) id!: string;
+
+  @Input({ required: true }) user!: {
+    id: string;
+    avatar: string;
+    name: string;
+  };
+
+  //  This is how I handled the inputs one at a time
+  /*
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+  */
 
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-
-    this.select.emit(this.id);
-
+    this.select.emit(this.user.id);
   }
 }
