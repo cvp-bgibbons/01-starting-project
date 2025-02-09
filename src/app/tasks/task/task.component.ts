fd4a8f6d831604beb9d86bbe247fea1ee647fe20
CISC181 <bgibbons@udel.edu>
2025-02-09 11:53:17 -0500

    format data using pipes

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Component,Input } from '@angular/core';
 import { Task } from './Task.model';
 import { CardComponent } from "../../shared/card/card.component";
@@ -5,7 +6,7 @@ import { CardComponent } from "../../shared/card/card.component";
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
